a23d10a3894456de8c6a57b1c3c12346fac198ef
Carl Molina <carlandrew.molina@gmail.com>
2023-12-05 12:34:15 -0800

    After First Code Review

--- queries.sql
+++ queries.sql
@@ -4,6 +4,7 @@
 SELECT *
 FROM owners
 LEFT JOIN vehicles AS v
+-- ^FIXME: FULL JOIN here instead
 ON v.owner_id = owners.id
 ORDER BY owners.id ASC;
 
@@ -14,13 +15,18 @@ FROM vehicles
 INNER JOIN owners
 ON owners.id = vehicles.owner_id
 GROUP BY first_name
-vehicles-# ORDER BY first_name ASC;
+-- ^FIXME: GROUP BY id here to be specific per ID
+ORDER BY first_name ASC;
 
 -- #3
 SELECT first_name, ROUND(AVG(price)), COUNT(*)
 FROM vehicles
-INNER JOIN owners as o
-ON vehicles.owner_id = o.id
+    INNER JOIN owners AS o
+        ON vehicles.owner_id = o.id
+        -- ^ proper indentation for readability
 GROUP BY first_name
-HAVING AVG(vehicles.price) > 10000 AND COUNT(*) > 1
+-- ^ FIXME: GROUP BY ID here to be specific per id
+HAVING AVG(vehicles.price) > 10000
+    AND COUNT(*) > 1
+    -- ^ better readability
 ORDER BY first_name DESC;
